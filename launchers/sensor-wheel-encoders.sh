4e87f3411103297f565f189d89a4c9e1a028248e
eldgar <walikanani@gmail.com>
2025-09-17 23:07:41 -0600

    updated for unique node names

--- launchers/sensor-wheel-encoders.sh
+++ launchers/sensor-wheel-encoders.sh
@@ -2,7 +2,7 @@
 source /environment.sh
 dt-launchfile-init --quiet
 
-dt-exec ros2 run wheel_encoder_driver wheel_encoder_driver_node -p wheel:=left &
-dt-exec ros2 run wheel_encoder_driver wheel_encoder_driver_node -p wheel:=right
+dt-exec ros2 run wheel_encoder_driver wheel_encoder_driver_node -p wheel:=left  --ros-args -r __node:=wheel_encoder_left  &
+dt-exec ros2 run wheel_encoder_driver wheel_encoder_driver_node -p wheel:=right --ros-args -r __node:=wheel_encoder_right
 
 dt-launchfile-join --quiet
